e753dd1b0f58559358c4a06053eaf310c176a6a6
Mads Zeneli <mads@zeneli.dk>
2020-01-29 08:54:08 +0100

    Adds internationalizer

---
+++ packages/internationalizer/src/index.ts
@@ -0,0 +1,1 @@
+export { default, useTranslation } from './Internationalizer';
---
+++ packages/internationalizer/src/types.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react';
+
+export interface Props {
+  config: object;
+  children: ReactNode;
+}
