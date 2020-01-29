ae1a994cab48d5fe0fa1c29cbfaf1a1eb5f092b5
Mads Zeneli <mads@zeneli.dk>
2020-01-29 09:27:14 +0100

    Updates export from internationalizer

--- packages/internationalizer/src/index.ts
+++ packages/internationalizer/src/index.ts
@@ -1,1 +1,27 @@
-export { default, useTranslation } from './Internationalizer';
+import i18n from 'i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { useTranslation as useTranslationHook, initReactI18next } from 'react-i18next';
+
+export const useTranslation = useTranslationHook;
+
+export const initialize = (
+  config: object = {
+    fallbackLng: 'en',
+    debug: false,
+    lowerCaseLng: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    nonExplicitWhitelist: true,
+  },
+) => {
+  i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init(config);
+  return i18n;
+};
+
+export default initialize;
--- packages/internationalizer/src/types.ts
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-
-export interface Props {
-  config: object;
-  children: ReactNode;
-}
