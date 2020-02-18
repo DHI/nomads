933de460ad2586089db6fe2b17da761de35245ac
Mads Zeneli <mads@zeneli.dk>
2020-02-18 16:27:58 +0100

    Adds marker docs

--- packages/mapbox/docs/Markers/Markers.stories.tsx
+++ packages/mapbox/docs/Markers/Markers.stories.tsx
@@ -6,20 +6,28 @@ export const Markers = () => (
   <div style={{ width: '100%', height: '500px' }}>
     <Mapbox
       mapPosition={{
-        latitude: 54.50252689212371,
-        longitude: 11.227997839450836,
+        latitude: 55.8714948,
+        longitude: 12.4934543,
         zoom: 8,
       }}
       mapStyle="mapbox://styles/mzdk/ck34cntqj05z91cmjvsjyfw4s"
       mapboxApiAccessToken="pk.eyJ1IjoibXpkayIsImEiOiJjazJuZTlobWkwcGc5M21sbml2cWd0Njl5In0.BasnEPdX95SqEnvv2AOjoQ"
       mapMarkers={[
-        <Marker
-          latitude={37.78}
-          longitude={-122.41}
-          offsetLeft={-20}
-          offsetTop={-10}
-        >
-          <div>You are here</div>
+        <Marker latitude={55.8714948} longitude={12.4934543}>
+          <div
+            style={{
+              backgroundColor: 'green',
+              color: 'white',
+              width: '50px',
+              height: '50px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '50px',
+            }}
+          >
+            DHI
+          </div>
         </Marker>,
       ]}
     />
