8d448af6d9ec3d70f99edf98af1eb89a905ccca0
Mads Zeneli <mads@zeneli.dk>
2020-03-11 09:57:58 +0100

    Adds loginFromLocalStorage

--- packages/domain-services/src/authentication/index.ts
+++ packages/domain-services/src/authentication/index.ts
@@ -1,9 +1,11 @@
 import login from './login';
+import loginFromLocalStorage from './loginFromLocalStorage';
 import logout from './logout';
 import isLoggedIn from './isLoggedIn';
 
 export default {
   login,
+  loginFromLocalStorage,
   logout,
   isLoggedIn,
 };
